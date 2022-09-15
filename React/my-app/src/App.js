c18f3dbca99fa2d8f578460b3e2562870636d9fd
sen <sen121108@icloud.com>
2022-09-15 10:10:54 +0900

    firstCommit

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+const[count, setCount] = useState(0);
 
-export default App;
+const onClickCountUp = () => {
+    setCount(count + 1); 
+};
+
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <p> {count} </p>
+        <button onClick={onClickCountUp} >CountUP</button>
+        <h3>test</h3>
+      </div>
+    );
+  };
